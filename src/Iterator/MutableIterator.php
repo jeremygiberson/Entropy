ff2ac6bd252744467c10cefb627816c781e04080
jeremygiberson@gmail.com <jeremygiberson@gmail.com>
2015-02-28 11:04:29 -0700

    wrote an iterator that you can mutate while iterating over

---
+++ src/Iterator/MutableIterator.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * User: Jeremy
+ * Date: 2/28/2015
+ * Time: 9:56 AM
+ */
+
+namespace JeremyGiberson\Entropy\Iterator;
+
+
+class MutableIterator implements \Iterator, \ArrayAccess, \Countable
+{
+    /** @var  array */
+    protected $collection;
+    /** @var  array */
+    protected $previous_keys;
+    /** @var  array */
+    protected $next_keys;
+    /** @var  mixed */
+    protected $current_key;
+
+    /**
+     * @param array $collection
+     */
+    function __construct(array $collection = [])
+    {
+        $this->collection = $collection;
+        $this->rewind();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current()
+    {
+        return $this->collection[$this->current_key];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        $this->previous_keys[] = $this->current_key;
+        $this->current_key = array_shift($this->next_keys);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        return $this->current_key;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        return isset($this->collection[$this->current_key]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        $this->previous_keys = [];
+        $this->next_keys = array_keys($this->collection);
+        $this->current_key = array_shift($this->next_keys);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->collection[$offset]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->collection[$offset];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->collection[$offset] = $value;
+        // if new key, add to end
+        if(!in_array($offset, $this->next_keys)) {
+            $this->next_keys[] = $offset;
+            // if we were at an invalid position (end) then
+            // set current position to the last element
+            if(!$this->valid())
+            {
+                $this->next();
+            }
+        }
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->collection[$offset]);
+        // if we've yet iterated over the key, remove it from pending iteration
+        if(in_array($offset, $this->next_keys))
+        {
+            $index = array_search($offset, $this->next_keys);
+            unset($this->next_keys[$index]);
+        } elseif (in_array($offset, $this->previous_keys))
+        {
+            // doesn't affect iterating, but for cleanup sake
+            $index = array_search($offset, $this->previous_keys);
+            unset($this->previous_keys[$index]);
+        }
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        return count($this->collection);
+    }
+
+    /**
+     * Return the first key of the iterator
+     * @return mixed
+     */
+    public function first_key()
+    {
+        reset($this->collection);
+        return key($this->collection);
+    }
+
+    /**
+     * Return the first value of the iterator
+     * @return mixed
+     */
+    public function first()
+    {
+        reset($this->collection);
+        return current($this->collection);
+    }
+
+    /**
+     * Return the last key of the iterator
+     * @return mixed
+     */
+    public function last_key()
+    {
+        end($this->collection);
+        return key($this->collection);
+    }
+
+    /**
+     * Return the last value of the iterator
+     * @return mixed
+     */
+    public function last()
+    {
+        end($this->collection);
+        return current($this->collection);
+    }
+}
---
+++ tests/Iterator/MutableIteratorTest.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * User: Jeremy
+ * Date: 2/28/2015
+ * Time: 10:10 AM
+ */
+
+namespace Entropy\Tests\Iterator;
+
+
+use JeremyGiberson\Entropy\Iterator\MutableIterator;
+
+class MutableIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_foreach()
+    {
+        $iterator = new MutableIterator(range(1,10));
+        $str = '';
+        foreach($iterator as $value)
+        {
+            $str .= $value;
+        }
+        $this->assertEquals('12345678910', $str,
+            'foreach should iterate over the collection');
+    }
+
+    public function test_count()
+    {
+        $iterator = new MutableIterator(range(1, $count = rand(2,10)));
+        $this->assertEquals($count, count($iterator),
+            'count should return number of items in iterator');
+    }
+
+    public function test_isset()
+    {
+        $iterator = new MutableIterator(['foo' => 'bar']);
+        $this->assertTrue(isset($iterator['foo']),
+            'isset should return true for a key in the iterator');
+        $this->assertFalse(isset($iterator['bar']),
+            'isset should return false for a key not in the iterator');
+    }
+
+    /**
+     * @depends test_isset
+     */
+    public function test_unset()
+    {
+        $iterator = new MutableIterator(['foo' => 'bar']);
+        $this->assertTrue(isset($iterator['foo']),
+            'isset should return true for a key in the iterator');
+        unset($iterator['foo']);
+        $this->assertFalse(isset($iterator['foo']),
+            'isset should return false for a key no longer in the iterator');
+    }
+
+    public function test_offsetGet()
+    {
+        $iterator = new MutableIterator(['foo' => $v = uniqid()]);
+        $this->assertEquals($v, $iterator['foo']);
+    }
+
+    /**
+     * @depends test_isset
+     * @depends test_offsetGet
+     */
+    public function test_offsetSet()
+    {
+        $iterator = new MutableIterator();
+        $this->assertFalse(isset($iterator['foo']),
+            'item should not be in the iterator');
+        $iterator['foo'] = $v = uniqid();
+        $this->assertEquals($v, $iterator['foo'],
+            'item should be in the iterator');
+    }
+
+    public function test_first_and_first_key()
+    {
+        $iterator = new MutableIterator(['foo' => 'bar', 'biz' => 'buz']);
+        $this->assertEquals('foo', $iterator->first_key(),
+            'should return first key of iterator');
+        $this->assertEquals('bar', $iterator->first(),
+            'should return first value of the iterator');
+    }
+
+    public function test_last_and_last_key()
+    {
+        $iterator = new MutableIterator(['foo' => 'bar', 'biz' => 'buz']);
+        $this->assertEquals('biz', $iterator->last_key(),
+            'should return last key of iterator');
+        $this->assertEquals('buz', $iterator->last(),
+            'should return last value of an iterator');
+    }
+
+    /**
+     * @depends test_count
+     * @depends test_unset
+     */
+    public function test_foreach_mutating_behind()
+    {
+        $iterator = new MutableIterator(range(1,10));
+        $lastKey = null;
+        $iterations = 0;
+        foreach($iterator as $key => $value)
+        {
+            $iterations++;
+            if($lastKey !== null)
+            {
+                unset($iterator[$lastKey]);
+            }
+            $lastKey = $key;
+        }
+
+        $this->assertEquals(10, $iterations,
+            'mutation should not have affected number of iterations');
+        $this->assertCount(1, $iterator,
+            'all but the last item should be removed from iterator');
+    }
+
+    /**
+     * @depends test_count
+     * @depends test_unset
+     */
+    public function test_foreach_mutating_current()
+    {
+        $iterator = new MutableIterator(range(1,10));
+        $iterations = 0;
+        foreach($iterator as $key => $value)
+        {
+            $iterations++;
+            unset($iterator[$key]);
+        }
+        $this->assertEquals(10, $iterations,
+            'mutation should not have affected number of iterations');
+        $this->assertCount(0, $iterator,
+            'all items should be removed from the iterator');
+    }
+
+    /**
+     * @depends test_count
+     * @depends test_unset
+     */
+    public function test_foreach_mutating_ahead()
+    {
+        $iterator = new MutableIterator(range(1,10));
+        $iterations = 0;
+        foreach($iterator as $key => $value)
+        {
+            $iterations++;
+            if($key != $iterator->last_key())
+            {
+                unset($iterator[$key + 1]);
+            }
+        }
+        $this->assertEquals(5, $iterations,
+            'mutation should have affected number of iterations');
+        $this->assertCount(5, $iterator,
+            'half the items should be removed from the iterator');
+    }
+}
